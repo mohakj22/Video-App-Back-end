4e42ce856d3a91d811170dd93d02d10ae52fb2b9
mohakj22 <mohakjain.ps12@gmail.com>
2024-07-28 18:41:42 +0530

    connected DB

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectBB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n Mongo DB connected!! \n DB connection instance: ${connectionInstance}`);
+        console.log(`\n Mongo DB connected!! \n DB Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Mongo DB connection error", error);
+        process.exit(1);
+    }
+}
+
+connectBB();
+
+export default connectBB;
