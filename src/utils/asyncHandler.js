777ac33affb13d007da3999807248b95bb709d5c
mohakj22 <mohakjain.ps12@gmail.com>
2024-07-31 23:09:08 +0530

    handling api errors

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((error) => {
+                next(error);
+            });
+    };
+};
+
+
+// Try Catch method => 
+
+
+/*
+const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        try {
+            await requestHandler(req, res, next);
+        } catch (error) {
+            res.status(error.code ?? 500).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    };
+};
+*/
+
+
+export default asyncHandler;
